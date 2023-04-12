8c61ce3fb9c59da54ef41fa13aa12136d36518a5
Leon Linhart <themrmilchmann@gmail.com>
2023-04-13 00:29:38 +0200

    feat: use catchall route for articles

---
+++ pages/articles/[...slug].vue
@@ -0,0 +1,17 @@
+<template>
+    <ContentDoc :path="$route.params.slug ? `/articles/${$route.params.slug[0]}` : '/'">
+        <template #not-found>
+            <h2>Blog slug ({{ $route.params.slug }}) not found</h2>
+        </template>
+    </ContentDoc>
+</template>
+
+<script>
+export default {
+    name: "_slug"
+}
+</script>
+
+<style scoped>
+
+</style>
