7574e736e21b8ed88c54a93ff9b7b0639c25c53d
Leon Linhart <themrmilchmann@gmail.com>
2023-04-13 21:03:32 +0200

    build: remove invalid configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    routeRoles: {
-        "/**": {
-            ssr: false,
-            prerendering: true
-        }
-    },
-
     modules: [
         "@nuxt/content"
     ],
