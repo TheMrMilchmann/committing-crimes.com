65616daef341ea470e590bdb46bb9e3e43617190
Leon Linhart <themrmilchmann@gmail.com>
2023-04-13 20:49:36 +0200

    build: configure prerendering

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    routeRoles: {
+        "/**": {
+            ssr: false,
+            prerendering: true
+        }
+    },
+
     modules: [
         "@nuxt/content"
     ],
