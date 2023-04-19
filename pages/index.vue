d868838d6839c0cb357b8821a6d9f4a2f0432479
Leon Linhart <themrmilchmann@gmail.com>
2023-04-19 23:26:23 +0200

    feat: add links to all articles

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,32 @@
     <div>
         <p>Hello World</p>
     </div>
+
+    <ContentList :query="articlesQuery" v-slot="{ list }">
+        <div v-for="article in list" :key="article._path">
+            {{ formatDateString(article.date) }}
+
+            <NuxtLink :href="article._path">
+                {{ article.title }}
+            </NuxtLink>
+        </div>
+    </ContentList>
 </template>
 
-<script>
-export default {
-    name: "index"
+<script setup lang="ts">
+import type {QueryBuilderParams} from "@nuxt/content/dist/runtime/types"
+import {format, parseISO} from "date-fns"
+import { ref } from "vue"
+
+const list = ref()
+
+const articlesQuery: QueryBuilderParams = {
+    path: "/articles",
+    sort: [{ date: -1 }]
+};
+
+function formatDateString(date: string): string {
+    return format(parseISO(date), "yyyy-MM-dd");
 }
 </script>
 
