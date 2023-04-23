2fbb05e9c9c8255c428095a2d010151286347187
Leon Linhart <themrmilchmann@gmail.com>
2023-04-23 14:42:56 +0200

    fix: adjust for proper dark mode class

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,7 +23,7 @@ export default defineNuxtConfig({
             ],
             theme: {
                 default: "github-light",
-                dark: "github-dark"
+                "dark-mode": "github-dark"
             }
         },
         sources: {
