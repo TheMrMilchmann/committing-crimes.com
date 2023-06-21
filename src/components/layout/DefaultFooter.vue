bb3aeab4f945b772fb28c7af9bd3e771dc879296
Leon Linhart <themrmilchmann@gmail.com>
2023-06-21 13:35:01 +0200

    feat: add footer with social links

---
+++ src/components/layout/DefaultFooter.vue
@@ -0,0 +1,68 @@
+<!--
+  - Copyright (c) 2023 Leon Linhart
+  - All rights reserved.
+  -->
+<template>
+    <div class="footer-container">
+        <div class="footer-icon-row">
+            <a href="https://github.com/TheMrMilchmann" class="icon icon-github" />
+            <a href="https://www.linkedin.com/in/leon-linhart/" class="icon icon-linkedin" />
+            <a href="https://mastodon.social/@themrmilchmann" class="icon icon-mastodon" />
+            <a href="https://twitch.tv/themrmilchmann" class="icon icon-twitch" />
+            <a href="https://www.youtube.com/@TheMrMilchmann" class="icon icon-youtube" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.footer-container {
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+}
+
+.footer-icon-row {
+    display: flex;
+    flex-direction: row;
+    gap: 12px;
+}
+
+.icon {
+    width: 40px;
+    height: 40px;
+    background-image: url("@/assets/icons/linkedin.svg");
+    background-size: contain;
+    filter: invert(100%) brightness(50%);
+
+    transition-duration: 280ms;
+    transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+}
+
+.icon:hover {
+    filter: invert(100%) brightness(0%);
+}
+
+.dark-mode .icon:hover {
+    filter: invert(100%) brightness(100%);
+}
+
+.icon-github {
+    background-image: url("@/assets/icons/github.svg");
+}
+
+.icon-linkedin {
+    background-image: url("@/assets/icons/linkedin.svg");
+}
+
+.icon-mastodon {
+    background-image: url("@/assets/icons/mastodon.svg");
+}
+
+.icon-twitch {
+    background-image: url("@/assets/icons/twitch.svg");
+}
+
+.icon-youtube {
+    background-image: url("@/assets/icons/youtube.svg");
+}
+</style>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,23 +2,43 @@
   - Copyright (c) 2023 Leon Linhart
   - All rights reserved.
   -->
+<script setup>
+import DefaultFooter from "~/components/layout/DefaultFooter.vue";
+</script>
+
 <template>
-    <div class="row">
-        <div class="container">
+    <div class="layout-container">
+        <div class="layout-column">
             <slot />
+
+            <DefaultFooter class="footer" />
         </div>
     </div>
 </template>
 
 <style scoped>
-.row {
+.layout-container {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
     display: flex;
     flex-direction: column;
     align-items: center;
 }
 
-.container {
+.layout-column {
+    display: flex;
+    flex-direction: column;
     max-width: 920px;
     width: 100%;
+    height: 100%;
+}
+
+.footer {
+    margin-top: auto;
+    padding-top: 48px;
+    padding-bottom: 12px;
 }
 </style>
