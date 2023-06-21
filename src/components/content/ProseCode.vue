598eb00b7f2c3716ecf6b5349ea575fa052680aa
Leon Linhart <themrmilchmann@gmail.com>
2023-06-21 12:53:58 +0200

    feat: customize code blocks

---
+++ src/components/content/ProseCode.vue
@@ -0,0 +1,19 @@
+<!--
+  - Copyright (c) 2023 Leon Linhart
+  - All rights reserved.
+  -->
+<template>
+    <div class="code-block .monospaced">
+        <slot />
+    </div>
+</template>
+
+<style scoped>
+.code-block {
+    display: block;
+
+    overflow-x: auto;
+    scrollbar-width: thin;
+    scrollbar-color: #FFA657 transparent;
+}
+</style>
