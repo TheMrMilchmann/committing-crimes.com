c66638509963257326a3a9596b7a54ca348c1397
Leon Linhart <themrmilchmann@gmail.com>
2023-09-24 17:49:42 +0200

    feat: code block scroll bar improvements

--- src/components/content/ProseCode.vue
+++ src/components/content/ProseCode.vue
@@ -14,6 +14,32 @@
 
     overflow-x: auto;
     scrollbar-width: thin;
-    scrollbar-color: #FFA657 transparent;
+    scrollbar-color: var(--highlight-color) transparent;
 }
+
+.code-block:deep(pre) {
+    background-color: transparent;
+}
+
+.code-block::-webkit-scrollbar {
+    height: 11px;
+}
+
+.code-block::-webkit-scrollbar-track {
+    background: transparent;
+}
+
+.code-block::-webkit-scrollbar-thumb {
+    background: var(--highlight-color);
+}
+
+.code-block::-webkit-scrollbar-thumb:hover {
+    background: var(--highlight-color);
+}
+
+/*
+.code-block::-webkit-scrollbar-thumb:active {
+    background: red;
+}
+ */
 </style>
