d35a4c46702eba8289948ade7bac0888af4b0876
Leon Linhart <themrmilchmann@gmail.com>
2024-01-28 20:00:43 +0100

    chore: preload fonts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,14 @@ export default defineNuxtConfig({
 
     app: {
         baseURL: "/",
-        pageTransition: { name: "page", mode: "out-in" }
+        pageTransition: { name: "page", mode: "out-in" },
+
+        head: {
+            link: [
+                { rel: "preload", href: "/assets/fonts/Inter.var.woff2", as: "font", type: "font/woff2", crossorigin: "anonymous" },
+                { rel: "preload", href: "/assets/fonts/JetBrainsMono-Regular.woff2", as: "font", type: "font/woff2", crossorigin: "anonymous" },
+            ]
+        }
     },
 
     colorMode: {
