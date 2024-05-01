df2e214da41170a44770685650e65bd6f1a53f13
Leon Linhart <themrmilchmann@gmail.com>
2024-05-02 00:42:08 +0200

    chore: improve line height in article paragraphs

---
+++ src/components/content/ProseP.vue
@@ -0,0 +1,13 @@
+<!--
+  - Copyright (c) 2023-2024 Leon Linhart
+  - All rights reserved.
+  -->
+<template>
+    <p class="paragraph"><slot /></p>
+</template>
+
+<style scoped>
+.paragraph {
+    line-height: 1.6em;
+}
+</style>
