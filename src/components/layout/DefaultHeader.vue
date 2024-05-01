000338f8c9743b82faf2fbafc3805ae8928f9e0a
Leon Linhart <themrmilchmann@gmail.com>
2024-05-02 01:35:13 +0200

    fix: remove non-existing links

--- src/components/layout/DefaultHeader.vue
+++ src/components/layout/DefaultHeader.vue
@@ -7,8 +7,6 @@
       <CCLink href="/"><span class="site-title monospaced">Committing Crimes</span></CCLink>
       <div class="navigation-row">
           <CCLink href="/">Articles</CCLink>
-          <CCLink href="/projects">Projects</CCLink>
-          <CCLink href="/preferences">Preferences</CCLink>
           <CCLink href="/about">About</CCLink>
       </div>
   </div>
