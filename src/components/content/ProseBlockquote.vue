c0b059c115de6e4a3a56aa05cbfbbb9e7712cb98
Leon Linhart <themrmilchmann@gmail.com>
2024-09-06 14:23:16 +0200

    feat: add custom styling for block quotes

---
+++ src/components/content/ProseBlockquote.vue
@@ -0,0 +1,21 @@
+<!--
+  - Copyright (c) 2023-2024 Leon Linhart
+  - All rights reserved.
+  -->
+<template>
+    <blockquote>
+        <slot />
+    </blockquote>
+</template>
+
+<style scoped>
+blockquote {
+    margin: 0;
+    padding-left: 20px;
+    border-left: 0.25rem solid var(--highlight-color);
+}
+
+blockquote > * {
+    opacity: 0.75;
+}
+</style>
