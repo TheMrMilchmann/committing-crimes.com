d525fe8aee6b788a1d314a58950c4ea71051fe67
Leon Linhart <themrmilchmann@gmail.com>
2024-12-22 22:19:49 +0100

    fix: configure line height for list items

---
+++ src/components/content/ProseLi.vue
@@ -0,0 +1,15 @@
+<!--
+  - Copyright (c) 2023-2024 Leon Linhart
+  - All rights reserved.
+  -->
+<template>
+    <li>
+        <slot />
+    </li>
+</template>
+
+<style scoped>
+li {
+    line-height: 1.6em;
+}
+</style>
